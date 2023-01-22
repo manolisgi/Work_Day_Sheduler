6d72cfc14516ccb18075808d82a6c3a8dac94a93
manolisgi <114882721+manolisgi@users.noreply.github.com>
2023-01-22 19:48:07 +0000

    added an interval to renew the seconds and  made bootstrap grid for the hours

--- starter/JS/script.js
+++ starter/JS/script.js
@@ -1,2 +1,25 @@
-var today = moment();
-$("#currentDay").text(today.format("dddd D MMM YYYY"));
+let note = localStorage.getItem("note");
+
+
+
+
+
+// Display the current day at the top of the calender when a user opens the planner
+setInterval(
+    function(){
+$("#currentDay").text(moment().format("dddd D/MMM/YYYY h:mm:ss "));
+}, 1000);
+
+
+// Create an "on-click" event attached to the "timeButton" id
+
+
+
+
+
+$(".saveBtn").on("click", function (event) {
+    event.preventDefault();
+    let note = $("#textarea").value;
+    localStorage.setItem("textarea", note);
+    console.log(note)
+})
